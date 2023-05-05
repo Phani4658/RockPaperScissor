21a540e8450045ce8605a7a8ea2caeba093a5085
Phani <mysteriousphani@gmail.com>
2023-05-05 21:16:00 +0530

    UI Design of RPS

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const rockBtn = document.getElementById('rockButton');
+const paperBtn = document.getElementById('paperButton');
+const scissorBtn = document.getElementById('scissorButton');
+const resultContainer = document.getElementById("result");
+let userScore = 0;
+let computerScore = 0;
+
 const choices = ["Rock", "Paper", "Scissor"];
 const getComputerChoice = () => {
   const randomIndex = Math.floor(Math.random() * 3);
@@ -5,32 +12,53 @@ const getComputerChoice = () => {
   return computerChoice;
 };
 
-const playRound = (computerChoice, userChoice) => {
+const playRound = (userChoice) => {
+  const computerChoice = getComputerChoice();
   const userWins =
-    (userChoice === "rock" && computerChoice === "scissor") ||
-    (userChoice === "paper" && computerChoice === "rock") ||
-    (userChoice === "scissor" && computerChoice === "paper");
+    (userChoice === "Rock" && computerChoice === "Scissor") ||
+    (userChoice === "Paper" && computerChoice === "Rock") ||
+    (userChoice === "Scissor" && computerChoice === "Paper");
   let result;
   if (userChoice === computerChoice) {
     result = "It's a Tie";
   } else if (userWins) {
     result = `You Won! ${userChoice} beats ${computerChoice}`;
+    userScore += 1;
   } else {
     result = `You Lost! ${computerChoice} beats ${userChoice}`;
+    computerScore += 1;
   }
-  return result;
+  addResult(result);
 };
 
-
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    const computerChoice = getComputerChoice().toLowerCase();
-    const userChoice = prompt(
-      "Enter Your Choice(Rock,Paper,Scissor): "
-    ).toLocaleLowerCase();
-    const roundResult = playRound(computerChoice, userChoice);
-    console.log(roundResult);
+function addResult(result){
+  const resultHeading = document.createElement('h3');
+  resultHeading.textContent = result;
+  resultContainer.appendChild(resultHeading);
+  const finalResult = document.createElement('h2');
+  if(userScore == 5){
+    finalResult.textContent = 'You Won';
+    resultContainer.append(finalResult);
+    userScore = 0;
+    computerScore = 0;
   }
-};
+  else if(computerScore == 5){
+    finalResult.textContent = 'You Lost';
+    resultContainer.appendChild(finalResult);
+    userScore = 0;
+    computerScore = 0;
+  }
+}
 
-game();
+rockBtn.addEventListener('click',() => {
+  playRound("Rock");
+})
+
+
+paperBtn.addEventListener('click',() => {
+  playRound("Paper");
+})
+
+scissorBtn.addEventListener('click',() => {
+  playRound("Scissor");
+})
